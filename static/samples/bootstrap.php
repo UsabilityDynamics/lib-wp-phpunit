97f8fcc33b36daddc3bbbdea4b39862888552f84
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-06-18 20:02:01 +0300

    update

--- static/samples/bootstrap.php
+++ static/samples/bootstrap.php
@@ -1,21 +1,37 @@
 <?php
 /**
- * PHP Unit Test
- * 
+ * PHP Unit Test Bootstrap
+ * This is bootstrap file which installs/loads bootstrap
  *
- * Direct call. example: http://{domain}/{path_to_module}/static/samples/bootstrap.php
+ * Note: you only have to set TEST_ROOT_PATH constant here.
  */
- 
+
+/** 
+ * REQUIRED. 
+ * SET TEST_ROOT_PATH CONSTANT. This is a path to root of your module 
+ */ 
+define( 'TEST_ROOT_PATH', 'PATH_TO_ROOT' );
+
+/** 
+ * OPTIONAL. 
+ * YOU CAN SET YOUR CUSTOM PATH TO CONFIG FILE HERE. 
+ *
+ * Note: default config file can be found in 'static/config/' directory.
+ */
+$config = false;
+
+/** DO NOT TOUCH CODE BELOW */
+
 // Set correct path to Composer Autoload file
-define( 'PHPUNIT_PROJECT_ROOT_PATH', dirname( dirname( dirname( __DIR__ ) ) ) );
-if( !file_exists( PHPUNIT_PROJECT_ROOT_PATH . '/vendor/libraries/autoload.php' ) || !require_once( PHPUNIT_PROJECT_ROOT_PATH . '/vendor/libraries/autoload.php' ) ) {
-  exit( "Could not load composer autoload file. Path: " . PHPUNIT_PROJECT_ROOT_PATH . '/vendor/libraries/autoload.php' . "\n" );
+$path = TEST_ROOT_PATH . '/vendor/autoload.php';
+if( !file_exists( $path ) || !require_once( $path ) ) {
+  exit( "Could not load composer autoload file. Path: {$path}\n" );
 }
 // Determine if our Bootstrap class exists.
 if( !class_exists( 'UsabilityDynamics\Test\Bootstrap' ) ) {
   exit( "Bootstrap class for init WP PHPUnit Tests is not found.\n" );
 }
 // Loader
-$test = UsabilityDynamics\Test\Bootstrap::get_instance();
-
-exit( "Done.\n" );
+UsabilityDynamics\Test\Bootstrap::get_instance( array( 
+  'config' => $config,  
+) );
